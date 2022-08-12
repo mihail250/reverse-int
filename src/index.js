578cd30901e2cdf78f11c8f04659c8fe2df3fe29
mihail250 <mmelihow@gmail.com>
2022-08-12 14:50:23 +0400

    add modul and () in 5 str

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 module.exports = function reverse (n) {
     var res = "";
+    var n = Math.abs(n);
     for (let i = n.toString().length; i > 0; i -= 1) {
-        res += n.toString[i];
+        res += n.toString()[i-1];
     }
     return res;
 
