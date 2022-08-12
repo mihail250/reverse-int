fa5685b7f4c1effc2f1b7eb9ef5caeb53384b4ca
mihail250 <mmelihow@gmail.com>
2022-08-12 14:30:41 +0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    var res = "";
+    for (let i = n.toString().length; i > 0; i -= 1) {
+        res += n.toString[i];
+    }
+    return res;
+
+};
